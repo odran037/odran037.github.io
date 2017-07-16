1783bfa186011d0dbf7e227a7bc061b877531e52
odran037 <hello@odran037.io>
2017-07-16 01:19:28 -0500

    ASDF

--- typeBanner.js
+++ typeBanner.js
@@ -1,13 +1,26 @@
 var banner = '\n           _                  ___ ____ ______ _\n          | |                / _ \\___ \\____  (_)\n  ___   __| |_ __ __ _ _ __ | | | |__) |  / / _  ___\n / _ \\ / _` | \'__/ _` | \'_ \\| | | |__ <  / / | |/ _ \\\n| (_) | (_| | | | (_| | | | | |_| |__) |/ / _| | (_) |\n \\___/ \\__,_|_|  \\__,_|_| |_|\\___/____//_/ (_)_|\\___/\n\n\n';
+var rant = ['\n\n\nI was born a dreamer.',
+            '\nA wad of cookie dough.',
+            '\n\nToday I am a man of action.',
+            '\nCarved out of wood.'];
 
-function typeOut(c, t) {
+function typeOut(c, t, el) {
   setTimeout(function() {
-    document.getElementById('banner-type').innerText += c;
+    document.getElementById(el).innerText += c;
   }, t * 50);
 }
 
 function start() {
-  for (var i = 0; i < banner.length; i++) {
-    typeOut(banner[i], i);
+  var j = 0;
+  for (var i = 0; i < banner.length + rant.length; i++) {
+    if (i < banner.length) {
+      typeOut(banner[i], i, 'banner-type');
+    } else {
+      typeOut(rant[j], i, 'rant');
+      j++;
+    }
+    // i === banner.length - 1 ?
+    // typeOut(banner[i], i, true) :
+    // typeOut(banner[i], i, false);
   }
 }
