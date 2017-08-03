70d3740d0e08d4b27a63d46f69e9f7524e6c1026
odran037 <hello@odran037.io>
2017-08-03 13:18:42 -0500

    asdf

--- typeBanner.js
+++ typeBanner.js
@@ -1,9 +1,8 @@
 var banner = '\n           _                  ___ ____ ______ _\n          | |                / _ \\___ \\____  (_)\n  ___   __| |_ __ __ _ _ __ | | | |__) |  / / _  ___\n / _ \\ / _` | \'__/ _` | \'_ \\| | | |__ <  / / | |/ _ \\\n| (_) | (_| | | | (_| | | | | |_| |__) |/ / _| | (_) |\n \\___/ \\__,_|_|  \\__,_|_| |_|\\___/____//_/ (_)_|\\___/\n\n\n';
-var rant = ['\n\n\n',
-            // 'I was born a dreamer.',
-            // '\nA wad of cookie dough.',
-            // '\n\nToday I am a man of action.',
-            // '\nCarved out of wood.\n\n\n',
+var rant = ['\n\n\nI was born a dreamer.',
+            '\nA wad of cookie dough.',
+            '\n\nToday I am a man of action.',
+            '\nCarved out of wood.\n\n\n',
             '<a href="https://github.com/odran037/contact-me"><i class="fa fa-paper-plane"></i></a>'];
 
 function typeOut(c, t, el) {
