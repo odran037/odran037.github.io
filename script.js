c3e9fd3ad11e2a2eaec11433261399748a53cc86
odran037 <leonardo.prates@outlook.com>
2017-12-03 20:57:34 -0600

    abominable :snowman:

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-var body = document.body;
-var div = document.createElement('div');
-var h1 = document.createElement('h1');
-var h1Text = document.createTextNode('odran');
-var small = document.createElement('small');
-var smallText = document.createTextNode('037');
-var br = document.createElement('br');
-var p = document.createElement('p');
-var pText = document.createTextNode('53662736772837');
-var links = [
+window.addEventListener('click', () => window.location.reload());
+
+const body = document.body;
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+const h1Text = document.createTextNode('odran');
+const small = document.createElement('small');
+const smallText = document.createTextNode('037');
+const br = document.createElement('br');
+const p = document.createElement('p');
+const pText = document.createTextNode('53662736772837');
+const links = [
   { title: 'github', url: 'https://github.com/odran037' },
   { title: 'codepen', url: 'https://codepen.io/odran037' },
   { title: 'glitch', url: 'https://glitch.com/@odran037' },
-  { title: 'refresh', url: false }
 ];
-var a = links.map(function(link) {
-  var tag = document.createElement('a');
-  var tagText = document.createTextNode(link.title);
+const a = links.map((link) => {
+  let tag = document.createElement('a');
+  let tagText = document.createTextNode(link.title);
+
   tag.appendChild(tagText);
-  if (link.url) {
-    tag.href = link.url;
-    tag.target = '_blank';
-  } else {
-    tag.href = '#';
-    tag.id = 'reload';
-  }
+  tag.href = link.url;
+  tag.target = '_blank';
+
   return tag;
 });
 
@@ -34,54 +32,62 @@ p.appendChild(pText);
 div.appendChild(h1);
 div.appendChild(br);
 div.appendChild(p);
-a.forEach(function(link) {
-  div.appendChild(link);
-});
+a.forEach((link) => div.appendChild(link) );
 body.appendChild(div);
 
-function getRandomCharacter(min, max) {
+
+const getRandomCharacter = (min, max) => {
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
 
-function generate(limit, min, max) {
-  var charactersList = [];
+
+const generate = (limit, min, max) => {
+
+  let charactersList = [];
 
   do {
     charactersList.push(String.fromCharCode(getRandomCharacter(min, max)));
   } while (--limit);
 
   return charactersList.join('');
+
 }
 
-setInterval(function() {
+
+setInterval(() => {
+
   for (let i = 0; i < 100; i++) {
-    setTimeout(function() {
+    setTimeout(() => {
       document.querySelector('p').innerText = generate(14, 48, 57);
     }, i * 10);
   }
 
-  setTimeout(function() {
+  setTimeout(() => {
     document.querySelector('p').innerText = '53662736772837';
   }, 1000);
+
 }, 5000);
 
-document.querySelectorAll('a').forEach(function(e) {
-  e.addEventListener('mouseenter', function() {
-    var initial = this.innerText;
-    var len = initial.length;
+
+document.querySelectorAll('a').forEach((e) => {
+
+  e.addEventListener('mouseenter', () => {
+
+    let initial = e.innerText;
+    let len = initial.length;
 
     for (let i = 0; i < 10; i++) {
-      setTimeout(function() {
+      setTimeout(() => {
         e.innerText = generate(len, 97, 122);
       }, i * 10);
     }
 
-    setTimeout(function() {
+    setTimeout(() => {
       e.innerText = initial;
     }, 100);
-  });
-});
 
-document.getElementById('reload').addEventListener('click', function() {
-  window.location.reload();
+  });
+
 });
