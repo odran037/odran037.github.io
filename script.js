15a3d5b7f616548e710116d480caceeba5b4e7f6
Leonardo Prates <odran037@users.noreply.github.com>
2021-06-16 21:08:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,53 @@
-window.onload = (icon) => {
-  let titleIcon = document.getElementById('title-icon');
-  let titleAlt = document.getElementById('title-alt');
-  let icons = ['github', 'gitlab', 'bitbucket'];
-  let host = window.location.hostname;
+const html = document.getElementsByTagName('html')[0]
+const head = document.createElement('head')
+const body = document.createElement('body')
+const elementsList = [
+  {
+    target: head,
+    elements: [
+      { name: 'meta', attributes: [{ name: 'charset', value: 'utf-8' }] },
+      { name: 'meta', attributes: [{ name: 'http-equiv', value: 'X-UA-Compatible' }, { name: 'content', value: 'IE=edge' }] },
+      { name: 'meta', attributes: [{ name: 'name', value: 'viewport' }, { name: 'content', value: 'width=device-width, initial-scale=1' }] },
+      { name: 'meta', attributes: [{ name: 'name', value: 'format-detection' }, { name: 'content', value: 'telephone=no' }] },
+      { name: 'title', properties: [{ name: 'textContent', value: '53662736772837' }] },
+      { name: 'link', attributes: [{ name: 'rel', value: 'shortcut icon' }, { name: 'href', value: 'favicon.ico' }, { name: 'type', value: 'image/x-icon' }] },
+      { name: 'link', attributes: [{ name: 'rel', value: 'stylesheet' }, { name: 'href', value: 'style.css' }] }
+    ]
+  },
+  {
+    target: body,
+    elements: [
+      { name: 'main', properties: [{ name: 'className', value: 'container mt-4 mb-4 pt-4 pb-4' }] },
+      { name: 'div', properties: [{ name: 'className', value: 'row justify-content-md-center mt-4 mb-4' }] },
+      { name: 'div', properties: [{ name: 'className', value: 'col col-lg-6 mt-4 mb-4' }] },
+      { name: 'p', properties: [{ name: 'textContent', value: 'Be in this life as if you were a stranger or a traveler on a path.' }] },
+      { name: 'p', properties: [{ name: 'textContent', value: 'If you reach the evening, do not expect to reach the morning, and if you reach the morning, do not expect to reach the evening. Take from your health before your sickness and from your life before your death.' }] }
+    ]
+  }
+]
 
-  titleIcon.textContent = icons.filter(icon => host.includes(icon))[0] || 'localhost';
+function domBuilder() {
+  elementsList.forEach(list => {
+    list.elements.forEach(e => {
+      let element = document.createElement(e.name)
 
-  // icons.forEach((icon) => {
-  //   if (host.includes(icon)) {
-  //     titleIcon.className = `fa fa-${icon}`;
-  //   } else {
-  //     titleIcon.textContent = 'localhost';
-  //     titleAlt.insertAdjacentHTML('beforeend', `<a href="https://odran037.${icon}.io"><i class="text-dark fa fa-${icon}"></i></a>`);
-  //   }
-  // });
+      if (e.hasOwnProperty('properties')) {
+        e.properties.forEach(prop => {
+          element[prop.name] = prop.value
+        })
+      }
+
+      if (e.hasOwnProperty('attributes')) {
+        e.attributes.forEach(prop => {
+          element.setAttribute(prop.name, prop.value)
+        })
+      }
+
+      list.target.appendChild(element)
+    })
+
+    html.appendChild(list.target)
+  })
 }
 
-const getRandomCharacter = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const generate = (limit, min, max) => {
-
-  let charactersList = [];
-
-  do {
-    charactersList.push(String.fromCharCode(getRandomCharacter(min, max)));
-  } while (--limit);
-
-  return charactersList.join('');
-
-};
-
-
-setInterval(function() {
-
-  let initial = document.querySelector('#n').getAttribute('data-initial');
-  let len = initial.length;
-
-  for (let i = 0; i < 100; i++) { 
-    setTimeout(() => {
-      let gen = generate(len, 48, 57);
-      document.querySelector('#n').innerText = gen;
-      document.title = gen;
-    }, i * 10);
-  }
-
-  setTimeout(() => {
-    document.querySelector('#n').innerText = initial;
-    document.title = initial;
-  }, 1000);
-
-}, 5000);
-
-document.querySelectorAll('.link').forEach(function(e) {
-
-  e.addEventListener('mouseenter', () => {
-
-    let initial = e.getAttribute('data-initial');
-    let len = initial.length;
-
-    for (let i = 0; i < 10; i++) {
-      setTimeout(() => {
-        e.innerText = generate(len, 97, 122);
-      }, i * 10);
-    }
-
-    setTimeout(() => {
-      e.innerText = initial;
-    }, 100);
-
-  });
-
-});
+domBuilder()
